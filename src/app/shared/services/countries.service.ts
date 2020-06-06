4d34b79834462a02dc3a04db7725c9202ee3756b
Enrique <enrique.delgado@live.com>
2020-06-06 17:26:46 -0500

    Add countries service

--- src/app/shared/services/countries.service.ts
+++ src/app/shared/services/countries.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { ICountry } from '../interfaces/country.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CountriesService {
+  private API_URL = 'https://restcountries.eu/rest/v2/all';
+  public countries$ = this.http.get<ICountry>(this.API_URL);
+
   constructor(private http: HttpClient) {}
 }
