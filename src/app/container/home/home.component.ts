8d0cede4c1a2caeca38bacd88a927f53add9ae42
Enrique <enrique.delgado@live.com>
2020-06-06 16:02:06 -0500

    Initialization of the reactive form

--- src/app/container/home/home.component.ts
+++ src/app/container/home/home.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  public userForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+    this.userForm = this.fb.group({
+      name: ['', Validators.required],
+      surname: ['', Validators.required],
+      email: ['', Validators.required],
+      countryId: ['', Validators.required],
+      gender: ['', Validators.required],
+      phone: ['', Validators.required],
+    });
   }
-
 }
